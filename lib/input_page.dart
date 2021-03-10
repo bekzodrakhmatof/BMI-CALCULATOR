74615b2a190e7ccccbb781229b347f591f585745
Bekzod <bekzod@thekeywe.com>
2021-03-10 22:27:18 +0900

    Initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

911b726d003704d47c77fc4ee8bae97e794002f7
Bekzod Rakhmatov <rakhmatovbekzod1996@gmail.com>
2021-03-11 14:01:12 +0900

    IconContent widget implemented

---
+++ lib/icon_content.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+
+const labelTextStyle = TextStyle(
+  fontSize: 18,
+  color: Color(0xFF8D8E98),
+);
+
+class IconContent extends StatelessWidget {
+
+  IconContent({this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          label,
+          style: labelTextStyle,
+        )
+      ],
+    );
+  }
+}
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF101E33);
+const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
 
@@ -15,46 +21,65 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text("BMI CALCULATOR"),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
                     child: ReusableCard(
-                      color: Color(0xFF101E33),
+                      color: activeCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: "MALE",
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: "FEMALE",
+                      ),
                     )
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    color: Color(0xFF101E33),
-                  )
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ReusableCard(
-              color: Color(0xFF101E33),
+            Expanded(
+              child: ReusableCard(
+                color: activeCardColor,
+              )
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                    )
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                    )
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: bottomContainerColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: double.infinity,
+              height: bottomContainerHeight,
             )
-          ),
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ReusableCard(
-                    color: Color(0xFF101E33),
-                  )
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    color: Color(0xFF101E33),
-                  )
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

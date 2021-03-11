fafdc5ee04f89f8a738c2078e56f4674dfeb7b24
Bekzod Rakhmatov <rakhmatovbekzod1996@gmail.com>
2021-03-11 11:11:37 +0900

    Refactored Widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'reusable_card.dart';
 
 class InputPage extends StatefulWidget {
 
@@ -14,15 +15,46 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text("BMI CALCULATOR"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF101E33),
+                    )
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: Color(0xFF101E33),
+                  )
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              color: Color(0xFF101E33),
+            )
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    color: Color(0xFF101E33),
+                  )
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: Color(0xFF101E33),
+                  )
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/reusable_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class ReusableCard extends StatelessWidget {
+
+  ReusableCard({@required this.color});
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        )
+    );
+  }
+}

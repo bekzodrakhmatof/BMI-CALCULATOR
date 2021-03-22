2fd97313053bbc3211f48d8c713e12bf5d4a1cda
Bekzod <bekzod@thekeywe.com>
2021-03-22 23:11:12 +0900

    Result page added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'icon_content.dart';
 import 'constants.dart';
+import 'results_page.dart';
 
 enum Gender {
   male,
@@ -210,14 +211,22 @@ class _InputPageState extends State<InputPage> {
                 ],
               ),
             ),
-            Container(
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: kBottomContainerColor,
-                borderRadius: BorderRadius.circular(10),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context, MaterialPageRoute(
+                    builder: (context) => ResultsPage()));
+              },
+              child: Container(
+                child: Text("CALCULATE"),
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: kBottomContainerColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: double.infinity,
+                height: kBottomContainerHeight,
               ),
-              width: double.infinity,
-              height: kBottomContainerHeight,
             )
           ],
         ),
